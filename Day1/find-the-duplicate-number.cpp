fb42d22c221df1de5c0c00b934cace80126b3da3
Fahad-copter <70465455+Fahad-copter@users.noreply.github.com>
2021-08-30 09:43:58 +0530

    day 1 problem 1 added
    in the name of dsa

---
+++ Day1/find-the-duplicate-number.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/find-the-duplicate-number/
+#include<bits/stdc++.h>
+using namespace std;
+class Solution{
+    public:
+    //we will be using hare n tortoise method
+            int findDuplicate(vector<int>& nums){
+                int slow = nums[0];
+                int fast = nums[0];
+                do {
+                    slow = nums[slow];
+                    fast = nums[nums[fast]];//for double speed
+                }
+                while(slow!=fast);
+                fast = nums[0];
+                while(slow!=fast){
+                    slow = nums[slow];
+                    fast = nums[fast];
+                }
+                return slow;//u can return fast also
+            }
+}
